27b990b2ead2b201839fecadcaddefff3d811b83
Глеб Щукин <g.shcukin@bizon.help>
2020-07-09 12:02:48 +0300

    test commit

--- src/js/Form.js
+++ src/js/Form.js
@@ -17,6 +17,7 @@
          * @param {Object|String} view Field view.
          * @param {Alpaca.Connector} connector Field connector.
          * @param {Function} errorCallback Error callback.
+		 * @test
          */
         constructor: function(domEl, options, viewId, connector, errorCallback) {
 
